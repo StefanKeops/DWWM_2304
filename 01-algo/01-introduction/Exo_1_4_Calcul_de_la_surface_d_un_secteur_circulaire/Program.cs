097c49e31d911aef3476666b41bf7ac73b040b42
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-04-25 16:47:53 +0200

    Exo 1.5

---
+++ 01-algo/01-introduction/Exo_1_4_Calcul_de_la_surface_d_un_secteur_circulaire/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace Exo_1_4_Calcul_de_la_surface_d_un_secteur_circulaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int R;
+            int A;
+            double pi = Math.PI;
+            double surface;
+            string saisie;
+
+            Console.WriteLine("Programme de calcul de la surface d'un secteur circulaire");
+
+            Console.WriteLine("Veuillez écrire la valeur du rayon R");
+
+                saisie = Console.ReadLine();
+
+                R = int.Parse(saisie);
+
+            Console.WriteLine("Veuillez écrire la valeur du angle A");
+
+                saisie = Console.ReadLine();
+
+                A = int.Parse(saisie);
+
+                surface = (pi * Math.Pow(R, 2) * A) / 360;
+
+            Console.WriteLine("La surface d'un secteur circulaire de rayon " + R + " cm et angle " + A + " degrés est de " + surface.ToString("#.##") + "cm².");
+
+            Console.ReadLine();
+        }
+    }
+}
