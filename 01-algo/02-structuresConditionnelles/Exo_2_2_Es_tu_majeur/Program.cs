8c2ce7b3a142f596ac3136df1831527453d8593e
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-04-26 11:24:38 +0200

    Exo 2.2

---
+++ 01-algo/02-structuresConditionnelles/Exo_2_2_Es_tu_majeur/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exo_2_2_Es_tu_majeur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int majeur = 18;
+            string saisie;
+
+            Console.WriteLine("Veuillez saisir votre age s'il vous plaît.");
+
+                saisie = Console.ReadLine();
+
+                age = int.Parse(saisie);
+
+            if (age >= majeur)
+            {
+
+                Console.WriteLine("Vous êtes majeur.");
+
+            }
+            else if (age < 0)
+            {
+
+                Console.WriteLine("Vous n’êtes pas encore né.");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Vous êtes mineur.");
+
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
