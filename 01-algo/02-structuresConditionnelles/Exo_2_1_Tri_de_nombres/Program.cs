313cfa57af6f79000912f473af90f56fce07b083
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-04-26 10:51:07 +0200

    Exo 2.1

---
+++ 01-algo/02-structuresConditionnelles/Exo_2_1_Tri_de_nombres/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exo_2_1_Tri_de_nombres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            string saisie;
+
+            Console.WriteLine("Programme Tri de nombres");
+
+            Console.WriteLine("Veuillez donner la valeur du A");
+
+               saisie = Console.ReadLine();
+
+               A = int.Parse(saisie);
+
+            Console.WriteLine("Veuillez donner la valeur du B");
+
+                saisie = Console.ReadLine();
+
+                B = int.Parse(saisie);
+
+            if (A > B)
+            {
+
+                Console.WriteLine("Afficher " + B + "," + A + ".");
+
+            }
+            else
+            { 
+
+                Console.WriteLine("Afficher " + A + "," + B + ".");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
