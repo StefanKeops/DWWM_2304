faa23a1d06787b81861010f78f49351a8c7f0ce9
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-04-26 16:57:18 +0200

    Exo 2.5 (partial)

---
+++ 01-algo/02-structuresConditionnelles/Exo_2_5_Ma_Bicyclete/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exo_2_5_Ma_Bicyclete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
