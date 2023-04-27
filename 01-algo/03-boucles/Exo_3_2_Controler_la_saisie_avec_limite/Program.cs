cd83cca829b3e23a5db7cf1113b77d81d7ff8907
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-04-27 16:59:53 +0200

    Exo 3.2

--- 01-algo/03-boucles/Exo_3_2_Controler_la_saisie_avec_limite/Program.cs
+++ 01-algo/03-boucles/Exo_3_2_Controler_la_saisie_avec_limite/Program.cs
@@ -8,6 +8,7 @@ namespace Exo_3_2_Controler_la_saisie_avec_limite
         {
             string password;
             int compteur = 0;
+            int tentative = 3;
 
             do
             {
@@ -17,18 +18,12 @@ namespace Exo_3_2_Controler_la_saisie_avec_limite
 
                 compteur++;
 
-                if (password == "formation") ;
+                while (password == "formation" && (tentative - compteur) == 0 ) ;
                 {
-                    if (compteur == 1)
-                    {
-                        Console.WriteLine("Mot de passe incorrect, vous devez réessayer ; vous avez encore 2 tentatives");
-                    }
-                    else if (compteur == 2)
-                    {
-                        Console.WriteLine("Mot de passe incorrect, vous devez réessayer ; vous avez encore 1 tentative");
-                    }
-                    else
-                    {
+                    Console.WriteLine("Mot de passe incorrect, vous devez réessayer ; vous avez encore " + (tentative - compteur) + " tentatives");
+                    { 
+                    while (password != "formation" && compteur = 3);
+
                         Console.WriteLine("Votre compte est bloqué");
                     }
                 }
