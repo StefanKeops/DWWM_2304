eb59834a76b32fecbf9bf541332565806e9ab6cb
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-05-02 11:57:14 +0200

    Exo 3.4

---
+++ 01-algo/03-boucles/Exo_3_4_Recherche_des_diviseurs_d_un_numbre/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exo_3_4_Recherche_des_diviseurs_d_un_numbre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre;
+            int diviseur;
+            string saisie;
+
+            Console.WriteLine("Introduisez la valeur d'un nombre");
+
+            saisie = Console.ReadLine();
+
+            nombre = int.Parse(saisie);
+
+            Console.WriteLine("Les diviseurs de " + nombre + " sont:");
+
+            for (diviseur = 1; diviseur < nombre; diviseur++)
+
+                if (nombre % diviseur == 0)
+                {
+                    Console.WriteLine(diviseur);
+                }
+        }
+    }
+}
