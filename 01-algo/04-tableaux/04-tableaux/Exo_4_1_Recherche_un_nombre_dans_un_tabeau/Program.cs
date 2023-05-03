6878d7f41a8fbaf49c87a5cea2e2b14f3f026597
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-05-03 14:42:32 +0200

    Exo 4.1

---
+++ 01-algo/04-tableaux/04-tableaux/Exo_4_1_Recherche_un_nombre_dans_un_tabeau/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Security;
+
+namespace Exo_4_1_Recherche_un_nombre_dans_un_tabeau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tableau = { 8, 16, 32, 64, 128, 256, 512 };
+            int indiceDebut = 0;
+            int indiceFin;
+            int indiceMilieu;
+            int n;
+            bool trouve = false;
+            string saisie;
+
+            Console.WriteLine("Saisissez le nombre à rechercher :");
+
+            saisie = Console.ReadLine();
+
+            n = int.Parse(saisie);
+
+            indiceFin = tableau.Length - 1;
+
+            while (indiceDebut <= indiceFin)
+            {
+                indiceMilieu = (indiceDebut + indiceFin) / 2;
+
+                if (tableau[indiceMilieu] == n)
+                {
+                    Console.WriteLine("Le numero est " + n);
+                    return;
+                }
+                else if (tableau[indiceMilieu] < n)
+                {
+                    indiceDebut = indiceMilieu + 1;
+                }
+                else
+                {
+                    indiceFin = indiceMilieu - 1;
+                }
+            }
+            Console.WriteLine("Nombre non trouvé");
+            }
+        }
+    }
