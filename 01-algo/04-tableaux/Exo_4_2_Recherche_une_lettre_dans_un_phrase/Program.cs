4d10dd18e3c5b5cc89c0e98634158b314eaf5528
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-05-03 16:14:13 +0200

    Exo 4.2

---
+++ 01-algo/04-tableaux/Exo_4_2_Recherche_une_lettre_dans_un_phrase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Channels;
+
+namespace Exo_4_2_Recherche_une_lettre_dans_un_phrase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string chaine = ".";
+
+            int index = chaine.IndexOf(" ");
+
+            if (chaine.Length == 0 || chaine.Length == 1 && index == -1)
+            {
+                Console.WriteLine("LA CHAINE EST VIDE");
+            }
+            else
+            {
+                Console.WriteLine(chaine);
+            }
+        }
+    }
+}
