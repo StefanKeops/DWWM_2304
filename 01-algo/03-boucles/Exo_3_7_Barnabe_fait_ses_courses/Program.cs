1c4de5ce005c5ad361e04e45b3331947d1b9917d
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-05-03 12:15:28 +0200

    Exo 3.7

---
+++ 01-algo/03-boucles/Exo_3_7_Barnabe_fait_ses_courses/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exo_3_7_Barnabe_fait_ses_courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+  ;
+            int nbMagasins = 0;
+            float solde;
+            string saisie;
+
+            Console.WriteLine("Introduisez la valeur d'un nombre");
+
+            saisie = Console.ReadLine();
+
+            solde = float.Parse(saisie);
+
+            while (solde >= (solde / 2+1)) //S>2
+            {
+                float depense = solde / 2 + 1;
+                solde = solde - depense;
+                nbMagasins++;
+            }
+            if (solde > 0)
+            {
+                    nbMagasins++;
+            }
+
+            // si j'ai encore de l'argent, je depense danss le dernier magasin
+
+            Console.WriteLine("Barnabé a acheté dans " + nbMagasins + " magasins.");
+        }
+    }
+}
