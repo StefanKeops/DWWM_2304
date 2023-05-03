620e080d7a19cbc81783ece0d1dfde205c3afad2
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-05-03 10:04:36 +0200

    Exo 3.6

---
+++ 01-algo/03-boucles/Exo_3_6_Jeu_de_la_Fourchette/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exo_3_6_Jeu_de_la_Fourchette
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombreRandom;
+            int nombreMystere;
+            int nombresEssais = 0;
+            string saisie;
+            
+            nombreRandom = new Random().Next(1, 100);
+
+            do
+            {
+                Console.WriteLine("Introduisez la valeur d'un nombre");
+
+                saisie = Console.ReadLine();
+
+                nombreMystere = int.Parse(saisie);
+
+                nombresEssais++;
+
+                if (nombreMystere < nombreRandom)
+                {
+                    Console.WriteLine("Le nombre saisi est trop petit.");
+                }
+                else if (nombreMystere > nombreRandom)
+                {
+                    Console.WriteLine("Le nombre saisi est trop grand.");
+                }
+            }
+            while (nombreMystere != nombreRandom);
+
+            Console.WriteLine("Bravo vous avez trouvé en " + nombresEssais + " essais.");
+
+
+        }
+    }
+}
