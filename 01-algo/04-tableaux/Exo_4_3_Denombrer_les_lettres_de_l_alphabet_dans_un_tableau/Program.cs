70abc8d58746bfff82d30463752ccabffb442414
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-05-04 12:07:00 +0200

    Exo 4.3

---
+++ 01-algo/04-tableaux/Exo_4_3_Denombrer_les_lettres_de_l_alphabet_dans_un_tableau/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exo_4_3_Denombrer_les_lettres_de_l_alphabet_dans_un_tableau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            int occurrence;
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            do
+            {
+                Console.WriteLine("Ecrivez un text d'au moins 120 caractere");
+
+                text = Console.ReadLine(); 
+            }
+            while (text.Length <= 120);
+            {
+                for (int letter = 0; letter < alphabet.Length; letter++)
+                {
+                    occurrence = 0;
+
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        if (text[i] == alphabet[letter])
+                        {
+                            occurrence++;
+                        }
+                    }
+
+                    Console.WriteLine("La lettre: " + alphabet[letter] + " est presente " + occurrence + " fois.");
+                }
+            }
+        }
+    }
+}
