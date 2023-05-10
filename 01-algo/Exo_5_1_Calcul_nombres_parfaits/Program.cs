513ca3055a11b2a462aed06a6e26e1c07c69a868
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-05-10 09:30:00 +0200

    Exo 5.1

---
+++ 01-algo/Exo_5_1_Calcul_nombres_parfaits/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exo_5_1_Calcul_nombres_parfaits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int compteur = 0;
+            int nombre = 2;
+            
+            while (compteur < 4)
+            {
+                int sommeDiviseurs = 1; // 1 est le diviseur de toutes les nombres
+
+                for (int i = 2; i < nombre; i++)
+                {
+                    if (nombre % i == 0)
+                    {
+                        sommeDiviseurs += i;
+                    }
+                }
+
+                if (sommeDiviseurs == nombre)
+                {
+                    Console.WriteLine(nombre);
+                    compteur++;
+                }
+
+                nombre++;
+            }
+
+        }
+    }
+}
