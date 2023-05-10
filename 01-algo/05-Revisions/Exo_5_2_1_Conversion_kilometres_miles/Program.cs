7a886f50b7b2e528a1033f66174ef69f2ff1cd21
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-05-10 12:16:55 +0200

    Exo 5.2.1

---
+++ 01-algo/05-Revisions/Exo_5_2_1_Conversion_kilometres_miles/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Exo_5_2_1_Conversion_kilometres_miles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valeur;
+            string saisie;
+            bool valeurAccepte = true;
+
+            do
+            {
+                Console.WriteLine("Entrez une valeur en kilomètres (0.01 à 1 000 000) ou 'q' pour quitter :");
+
+                saisie = Console.ReadLine();
+
+                if (saisie != "q")
+                {
+                    valeurAccepte = false;
+                }
+                else
+                {
+                    bool ok = double.TryParse(saisie, out valeur);
+                    if (!ok)
+                    {
+                        valeurAccepte = false;
+                    }
+                    else
+                    {
+                        if (valeur < 0.01 || valeur > 1000000)
+                        {
+                            valeurAccepte = false;
+
+                        }
+                        else
+                        {
+
+                            double miles = valeur / 1.609;
+
+                            Console.WriteLine("{0} kilomètres = {1:F2} miles", valeur, miles);
+                        }
+                    }
+                }
+            }
+            while ((saisie != "q"));
+        }
+    }
+}
