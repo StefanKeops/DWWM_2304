7495b8152847745cbbbd4f94074f0460b32c4a15
Pavan Kumar Sunkara <stephankeops@live.fr>
2023-05-12 23:16:27 +0200

    Exo 5.2.1

--- 01-algo/05-Revisions/Exo_5_2_1_Conversion_kilometres_miles/Program.cs
+++ 01-algo/05-Revisions/Exo_5_2_1_Conversion_kilometres_miles/Program.cs
@@ -18,33 +18,23 @@ namespace Exo_5_2_1_Conversion_kilometres_miles
 
                 if (saisie != "q")
                 {
-                    valeurAccepte = false;
-                }
-                else
-                {
                     bool ok = double.TryParse(saisie, out valeur);
-                    if (!ok)
+                    if (!ok || valeur < 0.01 || valeur > 1000000)
                     {
-                        valeurAccepte = false;
+                        Console.WriteLine("Valeur invalide.");
                     }
                     else
                     {
-                        if (valeur < 0.01 || valeur > 1000000)
-                        {
-                            valeurAccepte = false;
-
-                        }
-                        else
-                        {
-
-                            double miles = valeur / 1.609;
-
-                            Console.WriteLine("{0} kilomètres = {1:F2} miles", valeur, miles);
-                        }
+                        double miles = valeur / 1.609;
+                        Console.WriteLine("{0} kilomètres = {1:F2} miles", valeur, miles);
                     }
                 }
+                else
+                {
+                    valeurAccepte = false;
+                }
             }
-            while ((saisie != "q"));
+            while (valeurAccepte);
         }
     }
 }
