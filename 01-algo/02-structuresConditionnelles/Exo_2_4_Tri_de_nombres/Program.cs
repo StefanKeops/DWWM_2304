8917b1a796e8ada5042692d9c14feca168b0e1e7
Pavan Kumar Sunkara <stephankeops@live.fr>
2023-05-17 23:43:25 +0200

    Update Exo 4.2

--- 01-algo/02-structuresConditionnelles/Exo_2_4_Tri_de_nombres/Program.cs
+++ 01-algo/02-structuresConditionnelles/Exo_2_4_Tri_de_nombres/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Exo_2_4_Tri_de_nombres
+﻿using System;
+
+namespace Exo_2_4_Tri_de_nombres
 {
     internal class Program
     {
@@ -12,57 +14,56 @@
             Console.WriteLine("Programme Tri de trois nombres");
 
             Console.WriteLine("Veuillez donner la valeur du A");
-
             saisie = Console.ReadLine();
-
             A = int.Parse(saisie);
 
             Console.WriteLine("Veuillez donner la valeur du B");
-
             saisie = Console.ReadLine();
-
             B = int.Parse(saisie);
 
             Console.WriteLine("Veuillez donner la valeur du C");
-
             saisie = Console.ReadLine();
-
             C = int.Parse(saisie);
 
-            if (A > B)
+            if (A >= B && A >= C)
             {
-                if (B > C)
+                if (B >= C)
                 {
-                    Console.WriteLine("L'ordre decroissant est " + A + " , " + B + " , " + C + ".");
+                    Console.WriteLine("L'ordre décroissant est " + A + ", " + B + ", " + C + ".");
                 }
                 else
                 {
-                    Console.WriteLine("L'ordre decroissant est " + A + " , " + C + " , " + B + ".");
+                    Console.WriteLine("L'ordre décroissant est " + A + ", " + C + ", " + B + ".");
                 }
             }
-            else if (B > A)
+            else if (B >= A && B >= C)
             {
-                if (A > C)
+                if (A >= C)
                 {
-                    Console.WriteLine("L'ordre decroissant est " + B + " , " + A + " , " + C + ".");
+                    Console.WriteLine("L'ordre décroissant est " + B + ", " + A + ", " + C + ".");
                 }
                 else
                 {
-                    Console.WriteLine("L'ordre decroissant est " + B + " , " + C + " , " + A + ".");
+                    Console.WriteLine("L'ordre décroissant est " + B + ", " + C + ", " + A + ".");
                 }
             }
-            else
+            else if (C >= A && C >= B)
             {
-                if (A > B)
+                if (A >= B)
                 {
-                    Console.WriteLine("L'ordre decroissant est " + C + " , " + A + " , " + B + ".");
+                    Console.WriteLine("L'ordre décroissant est " + C + ", " + A + ", " + B + ".");
                 }
                 else
                 {
-                    Console.WriteLine("L'ordre decroissant est " + C + " , " + B + " , " + A + ".");
+                    Console.WriteLine("L'ordre décroissant est " + C + ", " + B + ", " + A + ".");
                 }
             }
-            Console.WriteLine();
         }
     }
 }
+
+
+
+
+
+
