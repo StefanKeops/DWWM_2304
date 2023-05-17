bca9a807f241578a18a82cc154530abd44943fa9
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-05-17 08:18:47 +0200

    update 17.05.23

---
+++ 01-algo/06-Revisions/Exo_6_1_Fruits_et_legumes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace Exo_6_1_Fruits_et_legumes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList legumes = new ArrayList();
+            string saisie;
+            bool valeurAccepte;
+
+            do
+            {
+                Console.WriteLine("Entrez un légume suivi de son prix au kilo (ex : carottes 2.99) ou tapez 'go' pour terminer : ");
+                saisie = Console.ReadLine();
+
+                if (saisie == "go")
+                {
+                    valeurAccepte = true;
+                }
+                else
+                {
+                    string[] elements = saisie.Split(" ");
+
+                    if (elements.Length != 2 || !double.TryParse(elements[1], out double prix))
+                    {
+                        Console.WriteLine("Valeur saisie invalide");
+                    }
+            }
+            
+    }
+}
---
+++ 01-algo/VolumeTerre/VolumeTerre/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace VolumeTerre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rayon;
+            string reponse;
+
+            Console.WriteLine("Veuillez saisir le rayon d'une sphère (La Terre)");
+
+            reponse = Console.ReadLine();
+            rayon = double.Parse(reponse);
+
+            double aire = 4 * Math.PI * Math.Pow(rayon, 2);
+
+            double volume = ( 4 * Math.PI * Math.Pow(rayon, 3) ) / 3;
+
+            Console.WriteLine("L'aire de la Terre est: {0:#,###.###} et son volume sera: {1:#,###.###}", aire, volume); 
+        }
+    }
+}
