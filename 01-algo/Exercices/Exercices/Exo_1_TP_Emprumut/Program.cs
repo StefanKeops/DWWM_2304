9175855f9bad3363cbbc97efe3d9478d8bc9a6ea
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-05-26 11:44:50 +0200

    Exo TP Emprumut

--- 01-algo/Exercices/Exercices/Exo_1_TP_Emprumut/Program.cs
+++ 01-algo/Exercices/Exercices/Exo_1_TP_Emprumut/Program.cs
@@ -7,12 +7,22 @@ namespace Exo_1_TP_Emprumut
     {
         static void Main(string[] args)
         {
-            double tauxInteretAnnuel =5;
-            int nombreAnneeRemboursement =4;
-            double capitalEmprunte =10000;
+            double tauxInteretAnnuel = 5;
+            int nombreAnneeRemboursement;
+            double capitalEmprunte;
+            string saisie;
+            string saisie2;
+
+            Console.WriteLine("Conseiller bancaire : Merci de nous indiquer le montant en euros que vous souhaitez emprunter");
+            saisie = Console.ReadLine();
+            capitalEmprunte = double.Parse(saisie);
+
+            Console.WriteLine("Conseiller bancaire : Merci de nous indiquer pour combien d'années vous souhaitez emprunter");
+            saisie2 = Console.ReadLine();
+            nombreAnneeRemboursement = int.Parse(saisie2);
 
             double mensualite = CalculMensualite(tauxInteretAnnuel, nombreAnneeRemboursement, capitalEmprunte);
-           // TableauAmortissementMensuel( tauxInteretAnnuel,  mensualite, capitalEmprunte, nombreAnneeRemboursement);
+            TableauAmortissementMensuel( tauxInteretAnnuel,  capitalEmprunte, nombreAnneeRemboursement, mensualite);
 
             Console.WriteLine("La mensualite constante est de " + mensualite.ToString("#.##") + " Euros");
         }
@@ -28,33 +38,39 @@ namespace Exo_1_TP_Emprumut
             return constanteMensualite;
         }
 
-        //public static void TableauAmortissementMensuel(double tauxInteretAnnuel, double capitalEmprunte, int nombreAnneeRemboursement)
-        //{
-        //    double tauxInteretMensuel = tauxInteretAnnuel / 1200;
-        //    int mois = nombreAnneeRemboursement * 1200;
-        //    double capitalRestant = capitalEmprunte;
+        public static void TableauAmortissementMensuel(double tauxInteretAnnuel, double capitalEmprunte, int nombreAnneeRemboursement, double mensualite)
+        {
+            double tauxInteretMensuel = tauxInteretAnnuel / 1200;
+            int mois = nombreAnneeRemboursement * 1200;
+            double capitalRestant = capitalEmprunte;
             
-        //    Console.WriteLine("Tableau d'amortissement:");
-        //    Console.WriteLine("Mois\tPart intérêt\tPart capital\tCapital restant\tMensualité");
+            Console.WriteLine("Tableau d'amortissement:");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("| Mois  | \tPart intérêt    | \tPart capital    | \tCapital restant | \tMensualité   |");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------");
 
-        //    for (int i = 0; i < mois; i++)
-        //    {
-        //        if (i == 0)
-        //        {
+            for (int i = 0; i < mois; i++)
+            {
+                double interetMensuel = capitalRestant * tauxInteretMensuel; 
+                double amortissementMois = mensualite - interetMensuel;
 
-        //        }
-        //        else
-        //        {
-        //            double interetMensuel = capitalRestant * tauxInteretMensuel;
-        //        double amortissementMois = mensualite - tauxInteretMensuel;
-        //        capitalRestant -= amortissementMois;
+                if (i == 0)
+                {
 
-        //        }
+                }
+                else
+                {
+                    capitalRestant -= amortissementMois;
+                }
 
-                
+                if (capitalRestant < 0)
+                {
+                    break;
+                }
 
-        //        Console.WriteLine((i + 1) + "\t" + interetMensuel.ToString("0.#") + "\t\t" + amortissementMois.ToString("0.#") + "\t\t" + capitalRestant.ToString("0") + "\t\t" + mensualite.ToString("#"));
-        //    }
-        //}
+                Console.WriteLine("|    " + (i + 1) + "\t|        " + interetMensuel.ToString("0.#") + "\t\t|        " + amortissementMois.ToString("0.#") + "\t\t|        " + capitalRestant.ToString("0") + "\t\t|        " + mensualite.ToString("#") + "           |");
+                Console.WriteLine("------------------------------------------------------------------------------------------------------");
+            }
+        }
     }
 }
