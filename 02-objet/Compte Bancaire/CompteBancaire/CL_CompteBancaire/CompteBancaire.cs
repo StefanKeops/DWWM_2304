e19504dc8d411671c1e812cd51b327adc2e25479
Pavan Kumar Sunkara <stephankeops@live.fr>
2023-06-25 13:03:27 +0200

    Compte Bancaire

--- 02-objet/Compte Bancaire/CompteBancaire/CL_CompteBancaire/CompteBancaire.cs
+++ 02-objet/Compte Bancaire/CompteBancaire/CL_CompteBancaire/CompteBancaire.cs
@@ -1,18 +1,20 @@
-﻿namespace CL_CompteBancaire
+﻿using System;
+
+namespace CL_CompteBancaire
 {
     public class CompteBancaire
     {
-        private int decouvertAutorize;
+        private int decouvertAutorise;
         private string nomProprietaire;
         private int numeroUniqueIdentification;
         private decimal solde;
 
         public CompteBancaire()
         {
-            CompteBancaire compte = new CompteBancaire();
-            CompteBancaire compte1 = new CompteBancaire(0123456789, "Stefan", 1500, -300);
-            Console.WriteLine(compte.ToString());
-            Console.WriteLine(compte1.ToString());
+            this.numeroUniqueIdentification = 0;
+            this.nomProprietaire = "";
+            this.solde = 0;
+            this.decouvertAutorise = 0;
         }
 
         public CompteBancaire(int numeroUniqueIdentification, string nomProprietaire, decimal solde, int decouvertAutorise)
@@ -20,13 +22,48 @@
             this.numeroUniqueIdentification = numeroUniqueIdentification;
             this.nomProprietaire = nomProprietaire;
             this.solde = solde;
-            this.decouvertAutorize = decouvertAutorise;
+            this.decouvertAutorise = decouvertAutorise;
         }
 
         public override string ToString()
         {
-            return "Numéro d'identification unique: " + numeroUniqueIdentification + "Nom proprietaire: " + nomProprietaire + "Solde: " + solde + "Decouvert autorise: " + decouvertAutorize;
+            return "Numéro d'identification unique: " + numeroUniqueIdentification + ", Nom propriétaire: " + nomProprietaire + ", Solde: " + solde + ", Découvert autorisé: " + decouvertAutorise;
         }
 
+        public void Crediter(decimal montant)
+        {
+            solde = this.solde + montant;
+        }
+
+        public void Debiter(decimal montant)
+        {
+            if (montant <= (solde + decouvertAutorise))
+            {
+                solde = this.solde - montant;
+            }
+            else
+            {
+                Console.WriteLine("Débit non autorisé : solde insuffisant.");
+            }
+        }
+
+        public void Transferer(CompteBancaire compteDestination, decimal montant)
+        {
+            if (montant <= (solde + decouvertAutorise))
+            {
+                Debiter(montant);
+                compteDestination.Crediter(montant);
+                Console.WriteLine("Transfert de " + montant + " effectué avec succès.");
+            }
+            else
+            {
+                Console.WriteLine("Transfert non autorisé : solde insuffisant.");
+            }
+        }
+
+        public bool Superieur(CompteBancaire autreCompte)
+        {
+            return solde > autreCompte.solde;
+        }
     }
 }
--- 02-objet/Compte Bancaire/CompteBancaire/ConsoleAppCompteBancaire/Program.cs
+++ 02-objet/Compte Bancaire/CompteBancaire/ConsoleAppCompteBancaire/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace ConsoleAppCompteBancaire
+﻿using CL_CompteBancaire;
+
+namespace ConsoleAppCompteBancaire
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-           
+            CompteBancaire compte = new CompteBancaire();
+            CompteBancaire compte1 = new CompteBancaire(123456789, "Stefan", 1500, -300);
+            Console.WriteLine(compte.ToString());
+            Console.WriteLine(compte1.ToString());
+
+            compte.Crediter(500);
+            Console.WriteLine("Après le crédit de 500 : " + compte1.ToString());
+
+            compte.Debiter(200);
+            Console.WriteLine("Après le débit de 200 : " + compte1.ToString());
+
+            compte.Debiter(3000); // Tentative de débit supérieur au solde autorisé
+            Console.WriteLine("Après le débit de 3000 : " + compte1.ToString());
+
+            CompteBancaire compte2 = new CompteBancaire(987654321, "Alice", 2000, -500);
+
+            Console.WriteLine("Compte 1 : " + compte1.ToString());
+            Console.WriteLine("Compte 2 : " + compte2.ToString());
+
+            compte1.Transferer(compte2, 500);
+
+            Console.WriteLine("Après le transfert de 500 de compte 1 à compte 2 :");
+            Console.WriteLine("Compte 1 : " + compte1.ToString());
+            Console.WriteLine("Compte 2 : " + compte2.ToString());
+
+            compte2.Transferer(compte1, 1000); // Tentative de transfert supérieur au solde autorisé
+
+            Console.WriteLine("Après le transfert de 1000 de compte 2 à compte 1 :");
+            Console.WriteLine("Compte 1 : " + compte1.ToString());
+            Console.WriteLine("Compte 2 : " + compte2.ToString());
+
+            Console.WriteLine("Compte 1 : " + compte1.ToString());
+            Console.WriteLine("Compte 2 : " + compte2.ToString());
+
+            bool estSuperieur = compte1.Superieur(compte2);
+            Console.WriteLine("Le solde du compte 1 est supérieur au solde du compte 2 : " + estSuperieur);
+
+            estSuperieur = compte2.Superieur(compte1);
+            Console.WriteLine("Le solde du compte 2 est supérieur au solde du compte 1 : " + estSuperieur);
         }
     }
 }
