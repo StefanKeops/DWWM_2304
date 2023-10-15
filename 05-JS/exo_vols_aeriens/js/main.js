97b333a6d9a55941e38eeb738f9deb89f14ffb50
Pavan Kumar Sunkara <stephankeops@live.fr>
2023-10-15 17:40:16 +0200

    update

--- 05-JS/exo_vols_aeriens/js/main.js
+++ 05-JS/exo_vols_aeriens/js/main.js
@@ -1,6 +1,7 @@
 const companySearchInput = document.getElementById("companySearch");
-const searchButton = document.getElementById("searchButton");
-const tableBody = document.getElementById("#vols tbody")
+const tableBody = document.querySelector("#vols tbody");
+const companyFilter = document.getElementById("companyFilter");
+
 let data = [];
 
 fetch("json/flights.json")
@@ -10,69 +11,62 @@ fetch("json/flights.json")
         }
         return response.json();
     })
-    .then(data => {
-        const tableBody = document.querySelector("#vols tbody");
-        data.forEach(item => {
-            const row = tableBody.insertRow();
-            row.insertCell().textContent = item.flight_id;
-            row.insertCell().textContent = item.start_city;
-            row.insertCell().textContent = item.arrival_city;
-            row.insertCell().textContent = item.start_time;
-            row.insertCell().textContent = item.arrival_time;
-            row.insertCell().textContent = item.flight_duration;
-            row.insertCell().textContent = item.airline_name;
-            row.insertCell().textContent = item.plane_maker;
-            row.insertCell().textContent = item.plane_ref;
-            row.insertCell().textContent = item.plane_type;
-            row.insertCell().textContent = item.plane_capacity;
-            row.insertCell().textContent = item.seats_free;
-        });
+    .then(flightsData => {
+        data = flightsData;
+        updateTable(data);
     })
     .catch(error => console.error("Error loading JSON data", error));
 
 companySearchInput.addEventListener("input", function () {
-    searchTerm = companySearchInput.value;
-})
+    const searchTerm = companySearchInput.value;
+    filterFlightsByCompany(searchTerm);
+});
 
-function filterFlightsByCompany(searcTerm) {
-    const tableBody = document.querySelector("#vols tbody");
+function filterFlightsByCompany(searchTerm) {
+    const filteredFlights = data.filter(item => {
+        const company = item.airline_name.toLowerCase();
+        return company.includes(searchTerm.toLowerCase());
+    });
+    updateTable(filteredFlights);
 }
 
-fetch("json/flights.json")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Invalid HTTP response");
-        }
-        return response.json();
-    })
+companyFilter.addEventListener("change", function () {
+    const selectedCompany = companyFilter.value;
+    if (selectedCompany === "") {
+        updateTable(data);
+    } else {
+        const filteredFlights = data.filter(item => item.airline_name === selectedCompany);
+        updateTable(filteredFlights);
+    }
+});
 
-    .then(flightsData => {
-    data = flightsData;
-    updateTable(data) // afisarea tuturor zborurilor
-})
-    
+function formatDuration(durationMinutes) {
+    const hours = Math.floor(durationMinutes / 60);
+    const minutes = durationMinutes % 60;
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:00`;
+}
+
 function updateTable(flights) {
     while (tableBody.firstChild) {
-        tableBody.removeChild(tableBody.firstChild) // eliminarea datelor din tabel
+        tableBody.removeChild(tableBody.firstChild);
     }
 
     flights.forEach(item => {
-
         const row = tableBody.insertRow();
-
         row.insertCell().textContent = item.flight_id;
         row.insertCell().textContent = item.start_city;
         row.insertCell().textContent = item.arrival_city;
         row.insertCell().textContent = item.start_time;
         row.insertCell().textContent = item.arrival_time;
-        row.insertCell().textContent = item.flight_duration;
+        
+        const durationCell = row.insertCell();
+        durationCell.textContent = formatDuration(item.flight_duration);
+
         row.insertCell().textContent = item.airline_name;
         row.insertCell().textContent = item.plane_maker;
         row.insertCell().textContent = item.plane_ref;
         row.insertCell().textContent = item.plane_type;
         row.insertCell().textContent = item.plane_capacity;
         row.insertCell().textContent = item.seats_free;
-
-    })
-    .catch(error => console.error("Error loading JSON data", error));
+    });
 }
