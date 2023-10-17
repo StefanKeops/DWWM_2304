73232c45ed3459cfa92d783d911376fa1d7c5a7f
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-10-17 12:49:23 +0200

    update employes

---
+++ 05-JS/exo_employes/js/main.js
@@ -0,0 +1,19 @@
+const apiUrl = "js/main.js"
+
+const { createApp } = Vue;
+
+const employeesApp = {
+    data () {
+        return {
+
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+
+createApp(employeesApp).mount('#app');
