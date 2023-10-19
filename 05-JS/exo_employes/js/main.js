e317609793880b8454b11a8aa07674c2346dd8c8
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-10-19 12:28:10 +0200

    update employees

--- 05-JS/exo_employes/js/main.js
+++ 05-JS/exo_employes/js/main.js
@@ -9,28 +9,62 @@ const { createApp } = Vue;
 const employeesApp = {
     data () {
         return {
-            employees: []
+            employees: [],
+            employeeCount: 0,
+            totalMonthlySalary: 0,
+            sortBy: 'asc',
         }
     },
     async mounted() {
         let json = await Db.fetchJson(apiUrl);
-         console.log(json)
-
+         
         for(let item of json.data) {
             let c = new Employee(item);
 
             c.email = createEmail(c.employee_name);
         
             this.employees.push(c);
+
+            this.totalMonthlySalary += parseFloat(this.monthlySalary(c));
         }
 
-        console.log(this.employees);
+        this.employeeCount = this.employees.lenght
     },
     methods: {
          monthlySalary(employee) {
             return (employee.employee_salary / 12) .toFixed(2)
         },
+
+        yearOfBirth(employee) {
+            const curentYear = new Date().getFullYear();
+            return curentYear - employee.employee_age;
+        },
+        
+        toggleSort() {
+            this.sortBy = this.sortBy === 'asc' ? 'desc' : 'asc';
+            this.employees.sort((a, b) => {
+              if (this.sortBy === 'asc') {
+                return a.employee_salary - b.employee_salary;
+              } else {
+                return b.employee_salary - a.employee_salary;
+              }
+            });
+        },
+
+        duplicateEmployee(employee) {
+            const duplicate = Object.assign({}, employee);
+            duplicate.id = duplicate.id + ".1";
+            this.employees.push(duplicate);
+        },
+
+        deleteEmployee(employee) {
+            const index = this.employees.indexOf(employee);
+
+            if (index !== -1) {
+                this.employees.splice(index, 1)
+            }
+        }
     }
-}
+  };
 
 createApp(employeesApp).mount('#app');
