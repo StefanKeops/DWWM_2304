b8fd4abbce923a1550ce984bd9c46c52726a9afc
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-10-26 16:49:30 +0200

    update

--- 05-JS/proiect program de facturi/js/main.js
+++ 05-JS/proiect program de facturi/js/main.js
@@ -11,7 +11,8 @@ const produitsApp = {
       return {
         produits: [],
         sortBy: 'asc',
-      };
+        nouveauProduit: { description: '', unite: '', prix: 0 }
+        };
     },
     async mounted() {
       let json = await Db.fetchJson(apiUrl);
@@ -19,10 +20,36 @@ const produitsApp = {
         let c = new Produit(item);
         this.produits.push(c);
       }
-    }
-};
-  
-  createApp(produitsApp).mount('#app');
+    },
+    methods: {
+      toggleSort() {
+        this.sortBy = this.sortBy === 'asc' ? 'desc' : 'asc';
+        this.produits.sort((a, b) => {
+          return this.sortBy === 'asc' ? a.description.localeCompare(b.description) : b.description.localeCompare(a.description);
+        });
+      },
 
-  
+      ajouterLigne(nouveauProduit) {
+        const dernierId = this.produits.length > 0 ?
+        Math.max(...this.produits.map(ID)): 0;
+        const produit = new Produit({
+          id: dernierId + 1,
+          description: nouveauProduit.description,
+          unite: nouveauProduit.unite,
+          prix: nouveauProduit.prix
+      })
+
+      this.produits.push(produit);
+
+      },
+
+      submitForm(event) {
+        event.preventDefault(); /* nu se trimite implicit formularul*/
+        this.ajouterLigne(this.nouveauProduit); /* adauga noul produs*/
+        this.nouveauProduit = { description: '', unite: '', prix: 0 } /* reinitializeaza formularul */
+      }
+    }
+}
  
+        
+  createApp(produitsApp).mount('#app');  
