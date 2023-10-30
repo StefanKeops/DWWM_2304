a631e8dd21018ce59a32353e36cb3df923c2788e
Pavan Kumar Sunkara <stephankeops@live.fr>
2023-10-30 01:06:35 +0100

    update facture

--- 05-JS/proiect program de facturi/js/main_factures.js
+++ 05-JS/proiect program de facturi/js/main_factures.js
@@ -17,11 +17,17 @@ const factureApp = {
         facture() {
             window.location.href = "facture.html";
         },
+        factureTVA() {
+            window.location.href = "factureTVA.html";
+        },
         liste1() {
             window.location.href = "liste_factures.html";
         },
         devis() {
             window.location.href = "devis.html";
+        },
+        devisTVA() {
+            window.location.href = "devisTVA.html";
         },
         liste2() {
             window.location.href = "liste_devises.html";
