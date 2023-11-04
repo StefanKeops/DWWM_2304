5cea2cd3dccbde9182ed861ecf39700d381c4645
Pavan Kumar Sunkara <stephankeops@live.fr>
2023-11-05 00:21:37 +0100

    evaluation

--- 05-JS/exo_class_grade/js/main.js
+++ 05-JS/exo_class_grade/js/main.js
@@ -15,6 +15,8 @@ const etudiantsApp = {
             },
             etudiantCount: 0,
             noteEliminatoare: 12,
+            moyenneClasse: 0,
+            etudiantsAuDessusMoyenne: 0,
         }
     },
     async mounted() {
@@ -24,39 +26,60 @@ const etudiantsApp = {
             return { nom, prenom, grade: item.grade };
         });
 
-        console.log(this.etudiants)
-
         this.etudiantCount = this.etudiants.length;
 
         const totalNotes = this.etudiants.reduce((total, etudiant) => total + etudiant.grade, 0);
         this.moyenneClasse = (totalNotes / this.etudiants.length).toFixed(2);
+
+        this.etudiantsAuDessusMoyenne = this.countEtudiantsAuDessusMoyenne();
     },
     methods: {
-
         ajouterEtudiant() {
-            if (this.nouveauEtudiant.fullname && this.nouveauEtudiant.grade) {
-                this.etudiants.push({
-                    nom: this.nouveauEtudiant.fullname.split(' ')[0],
-                    prenom: this.nouveauEtudiant.fullname.split(' ')[1],
-                    grade: parseInt(this.nouveauEtudiant.grade)
-                })
+            const nomPrenom = this.nouveauEtudiant.fullname.trim();
+            const grade = parseFloat(this.nouveauEtudiant.grade);
+
+            const nomPrenomPattern = /^[A-Z][a-z]+ [A-Z][a-z]+$/;
+            if (!nomPrenomPattern.test(nomPrenom)) {
+                alert("Le nom prénom doit être formaté correctement.");
+                return;
             }
 
-            this.sortTable()
-        },
+            if (isNaN(grade) || grade < 0 || grade > 20) {
+                alert("La note doit être un nombre compris entre 0 et 20.");
+                return;
+            }
 
+            const [nom, prenom] = nomPrenom.split(' ');
+            this.etudiants.push({
+                nom: nom.charAt(0).toUpperCase() + nom.slice(1).toLowerCase(),
+                prenom: prenom.charAt(0).toUpperCase() + prenom.slice(1).toLowerCase(),
+                grade: grade
+            });
+
+            const totalNotes = this.etudiants.reduce((total, etudiant) => total + etudiant.grade, 0);
+            this.moyenneClasse = (totalNotes / this.etudiants.length).toFixed(2);
+
+            this.sortTable();
+            this.etudiantCount = this.etudiants.length;
+
+            this.etudiantsAuDessusMoyenne = this.countEtudiantsAuDessusMoyenne();
+
+            this.nouveauEtudiant.fullname = '';
+            this.nouveauEtudiant.grade = 0;
+        },
+        
         sortTable() {
             this.etudiants.sort((a, b) => {
                 return b.grade - a.grade;
-              });
+            });
         },
-
+        
         countEtudiantsAuDessusMoyenne() {
-            const auDessusMoyenne = this.etudiants.filter(etudiant => etudiant.grade < this.moyenneClasse);
+            const auDessusMoyenne = this.etudiants.filter(etudiant => etudiant.grade > this.moyenneClasse);
+                console.log("Număr studenți deasupra mediei:", auDessusMoyenne.length);
             return auDessusMoyenne.length;
         },
-
-        }
-    };
+    }
+};
 
 createApp(etudiantsApp).mount('#app');
