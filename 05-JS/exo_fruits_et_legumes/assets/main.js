f6e87972fe219966c64be2b7dc8367bf03d65b09
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-11-21 08:55:58 +0100

    update

--- 05-JS/exo_fruits_et_legumes/assets/main.js
+++
@@ -1,9 +0,0 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            boxes: ['VENTES', 'NOUVELLE VENTE', 'LEGUMES', 'NOUVEAU LEGUME']
-        };
-    }
-});
-
-app.mount('#app');
