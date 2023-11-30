6ad226e6bbfdb3b3370bb8151603ad66ab596b80
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-11-30 15:19:37 +0100

    fonction PHP

--- 06-PHP/03 - fonctions/02-Mensualite_pret/calculMensualite.php
+++ 06-PHP/03 - fonctions/02-Mensualite_pret/calculMensualite.php
@@ -9,7 +9,19 @@ $annees = $_POST['annees'];
 
 // functia de calcul
 $mensualite = calculerMensualite($capital, $taux, $annees);
+$tableauAmortissement = calculerAmortissement($capital, $taux, $annees);
 
 // afisarea rezultatului
-echo "La mensualité constante du prêt est : " . round($mensualite, 2) . " €";
+echo "La mensualité constante du prêt est : " . round($mensualite, 2) . " €"; echo '<br>';
+echo '<table border="1">';
+echo '<tr><th>Mois</th><th>Intérêts</th><th>Partie amortissement</th><th>Capital restant dû</th></tr>';
+foreach ($tableauAmortissement as $ligne) {
+    echo '<tr>';
+    echo '<td>' . $ligne['mois'] . '</td>';
+    echo '<td>' . round($ligne['interets'], 2) . '</td>';
+    echo '<td>' . round($ligne['partAmortissement'], 2) . '</td>';
+    echo '<td>' . round($ligne['capitalRestant'], 2) . '</td>';
+    echo '</tr>';
+}
+echo '</table>'
 ?>
--- 06-PHP/03 - fonctions/02-Mensualite_pret/fonctions.php
+++ 06-PHP/03 - fonctions/02-Mensualite_pret/fonctions.php
@@ -1,5 +1,7 @@
 <?php
-function calculerMensualite($capital, $taux, $annees) {
+function CalculerMensualite(float $capital, float $taux, float $annees) : float
+
+{
     $tauxMensuel = $taux / 1200;
     $nombreMois = $annees * 12;
     $q = pow((1+ $tauxMensuel), -$nombreMois);
@@ -7,5 +9,35 @@ function calculerMensualite($capital, $taux, $annees) {
 
 
     return $mensualite;
+}
+
+function CalculerAmortissement(float $capital, float $taux, float $annees)
+
+{
+    $tauxMensuel = $taux / 1200;
+    $nombreMois = $annees * 12;
+    $mensualite = CalculerMensualite($capital, $taux, $annees);
+
+    $tableauAmortissement = array();
+
+    $capitalRestant = $capital;
+
+    for($mois = 1; $mois <= $nombreMois; $mois++) {
+        $interets = $capitalRestant * $tauxMensuel;
+        $partAmortissement = $mensualite - $interets;
+        $capitalRestant -= $partAmortissement;
+
+        $tableauAmortissement[] = array (
+            'mois' => $mois,
+            'interets' => $interets,
+            'partAmortissement' => $partAmortissement,
+            'capitalRestant' => abs($capitalRestant)
+        );
+
+    }
+
+
+
+    return $tableauAmortissement;
 }
 ?>
