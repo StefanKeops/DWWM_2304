3c71ccaae7bf6fe05d736280d54d97cacb836166
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-12-05 12:53:49 +0100

    PHP exo

---
+++ 06-PHP/00 - Exerxises/01 - Basic/006 - Browser detection script.php
@@ -0,0 +1,3 @@
+<?php
+echo "Votre navigateur est: " . $_SERVER ['HTTP_USER_AGENT']
+?>
---
+++ 06-PHP/00 - Exerxises/01 - Basic/012 - Check that emails are valid.php
@@ -0,0 +1,12 @@
+<?php
+// verifica daca e-mailul este valid/invalid
+$email = "mail@example.com";
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) 
+{
+     echo '"' . $email . '" = Valid'."\n";
+}
+else 
+{
+     echo '"' . $email . '" = Invalid'."\n";
+}
+?>
